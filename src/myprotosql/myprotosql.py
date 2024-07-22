d8022a550dbe6a3298822fbc2312bb3d154d3739
Janick Reynders <janick.reynders@gmail.com>
2024-07-22 20:31:08 +0200

    updated license

--- src/myprotosql/myprotosql.py
+++ src/myprotosql/myprotosql.py
@@ -1,8 +1,22 @@
+#  Copyright (c) 2024. Janick Reynders
+#
+#  This file is part of Myprotosql.
+#
+#  Myprotosql is free software: you can redistribute it and/or modify it under the terms of the
+#  GNU Lesser General Public License as published by the Free Software Foundation, either
+#  version 3 of the License, or (at your option) any later version.
+#
+#  Myprotosql is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+#  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#  See the GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License along with Myprotosql.
+#  If not, see <https://www.gnu.org/licenses/>.
+
 import json
 import sys
 
 from google.protobuf.compiler import plugin_pb2 as plugin
-from google.protobuf import descriptor_pb2
 from google.protobuf.json_format import MessageToJson
 
 
--- tests/test_messages_decode_raw.py
+++ tests/test_messages_decode_raw.py
@@ -1,4 +1,19 @@
 
+#  Copyright (c) 2024. Janick Reynders
+#
+#  This file is part of Myprotosql.
+#
+#  Myprotosql is free software: you can redistribute it and/or modify it under the terms of the
+#  GNU Lesser General Public License as published by the Free Software Foundation, either
+#  version 3 of the License, or (at your option) any later version.
+#
+#  Myprotosql is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+#  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#  See the GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License along with Myprotosql.
+#  If not, see <https://www.gnu.org/licenses/>.
+
 import json
 import simple_message_pb2
 import string_message_pb2
